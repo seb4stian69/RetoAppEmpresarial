baef09f2918a3cad60fea1a9d0610968ab385f8d
Sebastian Santis <sebastiandasanhe_1287@hotmail.com>
2022-09-03 15:12:52 -0500

    Añadiendo dependencia de Lombok + Añadiendo entendidades Jugador y Tablero

--- backend/domain/src/main/java/com/sofka/marvelgame/entities/Jugador.java
+++ backend/domain/src/main/java/com/sofka/marvelgame/entities/Jugador.java
@@ -1,4 +1,32 @@
 package com.sofka.marvelgame.entities;
 
-public class Jugador {
+import co.com.sofka.domain.generic.Entity;
+import com.sofka.marvelgame.values.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter/* */@Setter
+public class Jugador extends Entity<JugadorID> {
+
+    private final Alias alias;
+    private final Email email;
+    private final Puntos puntos;
+    private Mazo mazo;
+
+    public Jugador(JugadorID entityId, Alias alias, Email email, Puntos puntos, Mazo mazo) {
+        super(entityId);
+        this.alias = alias;
+        this.email = email;
+        this.puntos = puntos;
+        this.mazo = mazo;
+    }
+
+    public void agregarCartaAMazo(Carta carta){
+        mazo = mazo.agregarCarta(carta);
+    }
+
+    public void quitarCartaDeMazo(Carta carta){
+        mazo = mazo.retirarCarta(carta);
+    }
+
 }
--- backend/domain/src/main/java/com/sofka/marvelgame/entities/Tablero.java
+++ backend/domain/src/main/java/com/sofka/marvelgame/entities/Tablero.java
@@ -1,4 +1,63 @@
 package com.sofka.marvelgame.entities;
 
-public class Tablero {
+import co.com.sofka.domain.generic.Entity;
+import com.sofka.marvelgame.values.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+@Getter/* */@Setter
+public class Tablero extends Entity<TableroID> {
+
+    private Integer tiempoEnSegundos;
+    private Boolean estaHabilitado;
+    private final Map<JugadorID, Set<Carta>> partida;
+
+    public Tablero(TableroID entityId, Set<JugadorID> jugadorIds) {
+        super(entityId);
+        this.partida = new HashMap<>();
+        this.estaHabilitado = false;
+        jugadorIds.forEach(jugadorId -> partida.put(jugadorId, new HashSet<>()));
+    }
+
+    public void ajustarTiempo(Integer tiempo){
+        this.tiempoEnSegundos = tiempo;
+    }
+
+
+    public Integer tiempo() {
+        return tiempoEnSegundos;
+    }
+
+    public void adicionarPartida(JugadorID jugadorId, Carta carta){
+        partida.getOrDefault(jugadorId, new HashSet<>()).add(carta);
+    }
+
+    public void quitarCarta(JugadorID jugadorId, Carta carta){
+        partida.getOrDefault(jugadorId, new HashSet<>()).remove(carta);
+    }
+
+    public void habilitarApuesta(){
+        this.estaHabilitado = true;
+    }
+
+    public void inhabilitarApuesta(){
+        this.estaHabilitado = false;
+    }
+
+    public void reiniciarPartida(){
+        partida.clear();
+    }
+
+    public Boolean estaHabilitado() {
+        return estaHabilitado;
+    }
+
+    public Map<JugadorID, Set<Carta>> partida() {
+        return partida;
+    }
+
 }
