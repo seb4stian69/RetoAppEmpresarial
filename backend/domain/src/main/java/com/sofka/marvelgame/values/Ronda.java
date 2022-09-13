95d6c4b156c4ce9976e339c4c7db6ad2891289fb
Sebastian Santis <sebastiandasanhe_1287@hotmail.com>
2022-09-13 12:32:22 -0500

    Materializacion del tablero solucionada

--- backend/domain/src/main/java/com/sofka/marvelgame/values/Ronda.java
+++ backend/domain/src/main/java/com/sofka/marvelgame/values/Ronda.java
@@ -7,10 +7,11 @@ import java.util.logging.Logger;
 
 public class Ronda implements ValueObject<Ronda.Props> {
 
-    private final Logger log = Logger.getLogger("Logger");
     private final Set<JugadorID> jugadores;
-    private Boolean estaIniciada;
     private final Integer numero;
+    private final Boolean estaIniciada;
+
+
 
     public Ronda(Integer numero, Set<JugadorID> jugadores) {
         this.jugadores = jugadores;
@@ -18,36 +19,42 @@ public class Ronda implements ValueObject<Ronda.Props> {
         this.estaIniciada = false;
     }
 
-    private Ronda(Set<JugadorID> jugadores, Integer numero, Boolean estaIniciada) {
+    private Ronda(Integer numero, Set<JugadorID> jugadores, Boolean estaIniciada) {
         this.jugadores = jugadores;
         this.numero = numero;
         this.estaIniciada = estaIniciada;
     }
 
+
+
     public Ronda iniciarRonda(){
-        return new Ronda(jugadores, numero, true);
+        return new Ronda(this.numero, this.jugadores, true);
     }
 
     public Ronda terminarRonda(){
-        return new Ronda(jugadores, numero, false);
+        return new Ronda(this.numero, this.jugadores, false);
     }
 
+    //public Ronda
+
     public Ronda incrementarRonda(Set<JugadorID> jugadores){
-        log.info("Nueva ronda creada en el tablero");
-        return new Ronda(jugadores,this.numero + 1, false);
+        return new Ronda(this.numero + 1, jugadores, false);
     }
+
 
     @Override
     public Props value() {
-        return new Props(){
+        return new Props() {
             @Override
             public Set<JugadorID> jugadores() {
                 return jugadores;
             }
+
             @Override
             public Integer numero() {
                 return numero;
             }
+
             @Override
             public Boolean estaIniciada() {
                 return estaIniciada;
